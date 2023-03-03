0ff0be38cd57a471d8fec7e32e062a8187d89795
Stephen Alexandro Tjandra <stephen_tjandra@hotmail.com>
2023-03-03 19:28:23 +1100

    Add theme support

--- xmobar.hs
+++ xmobar.hs
@@ -1,7 +1,78 @@
 -- Compile this with something like
 -- stack ghc -- --make -o $HOME/.local/bin/xmobar xmobar.hs
 
+import Text.Printf (printf)
 import Xmobar
+
+data MyTheme = MT
+  { myBg :: String,
+    myFg :: String,
+    myRed :: String,
+    myColorDate :: String,
+    myColorMemory :: String,
+    myColorCpu :: String,
+    myColorVpnIp :: String
+  }
+
+-- Colors from https://github.com/catppuccin/catppuccin
+latteTheme :: MyTheme
+latteTheme =
+  MT
+    { myBg = "#eff1f5",
+      myFg = "#4c4f69",
+      myRed = "#d20f39",
+      myColorDate = "#dc8a78",
+      myColorMemory = "#dd7878",
+      myColorCpu = "#ea76cb",
+      myColorVpnIp = "#e64553"
+    }
+
+-- Colors from https://github.com/catppuccin/catppuccin
+macchiatoTheme :: MyTheme
+macchiatoTheme =
+  MT
+    { myBg = "#24273a",
+      myFg = "#cad3f5",
+      myRed = "#ed8796",
+      myColorDate = "#f5bde6",
+      myColorMemory = "#f5bde6",
+      myColorCpu = "#f5bde6",
+      myColorVpnIp = "#f5bde6"
+    }
+
+-- Colors from https://www.nordtheme.com/
+nordTheme :: MyTheme
+nordTheme =
+  MT
+    { myBg = "#2e3440",
+      myFg = "#d8dee9",
+      myRed = "#bf616a",
+      myColorDate = "#81a1c1",
+      myColorMemory = "#81a1c1",
+      myColorCpu = "#81a1c1",
+      myColorVpnIp = "#b48ead"
+    }
+
+-- Colors from https://github.com/Binaryify/OneDark-Pro
+onedarkTheme :: MyTheme
+onedarkTheme =
+  MT
+    { myBg = "#282c34",
+      myFg = "#abb2bf",
+      myRed = "#e05561",
+      myColorDate = "#42b3c2",
+      myColorMemory = "#42b3c2",
+      myColorCpu = "#42b3c2",
+      myColorVpnIp = "#42b3c2"
+    }
+
+-- The following line could be changed by ~/git/scripts-public/change_theme
+-- Please be careful when changing
+myTheme :: MyTheme
+myTheme = macchiatoTheme
+
+myXmobarColor :: String -> String -> String
+myXmobarColor color text = printf "<fc=%s>" color ++ text ++ "</fc>"
 
 config :: Config
 config =
@@ -9,8 +80,8 @@ config =
     { font = "xft:Fira Code:weight=bold:pixelsize=13:antialias=true:hinting=true",
       additionalFonts = ["xft:Font Awesome 6 Free Solid:style=Solid:pixelsize=12"],
       border = NoBorder,
-      bgColor = "#2e3440",
-      fgColor = "grey",
+      bgColor = myBg myTheme,
+      fgColor = myFg myTheme,
       alpha = 255,
       position = TopW L 100,
       textOffset = -1,
@@ -36,7 +107,7 @@ config =
                 "-H",
                 "50",
                 "--high",
-                "#bf616a"
+                myRed myTheme
               ]
               30,
           Run $
@@ -50,7 +121,7 @@ config =
                 "-H",
                 "80",
                 "--high",
-                "#bf616a"
+                myRed myTheme
               ]
               100,
           Run $ Com "/home/stephen/git/scripts-public/vpn_ip" [] "vpnip" 50,
@@ -59,13 +130,15 @@ config =
       sepChar = "%",
       alignSep = "}{",
       template =
-        "%XMonadLog%}\
-        \{\
-        \<fc=#b48ead>%vpnip%</fc>\
-        \<fc=#81a1c1>%cpu%</fc>   \
-        \<fc=#81a1c1>%memory%</fc>   \
-        \<fc=#81a1c1>%date%</fc> \
-        \%_XMONAD_TRAYPAD%"
+        "%XMonadLog%}{"
+          ++ myXmobarColor (myColorVpnIp myTheme) "%vpnip%"
+          ++ myXmobarColor (myColorCpu myTheme) "%cpu%"
+          ++ "   "
+          ++ myXmobarColor (myColorMemory myTheme) "%memory%"
+          ++ "   "
+          ++ myXmobarColor (myColorDate myTheme) "%date%"
+          ++ " "
+          ++ "%_XMONAD_TRAYPAD%"
     }
 
 main :: IO ()
